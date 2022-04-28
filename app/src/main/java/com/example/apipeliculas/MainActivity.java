be2b08ac2363a1df19bb0263a90878418c4101c2
MSI\WPOSS <gtamayoc@ufpso.edu.co>
2022-04-28 08:34:16 -0500

    PRIMER COMMIT - Archivos base de la api

---
+++ app/src/main/java/com/example/apipeliculas/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.apipeliculas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
